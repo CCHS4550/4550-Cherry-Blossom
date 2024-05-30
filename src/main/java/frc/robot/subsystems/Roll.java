794ae44284873c76cb1314951c445ce01bedd86b
RuhanRajasthan <rsharma@cherrycreekschools.org>
2024-05-30 10:33:38 -0600

    Created Roll.java, functions and command

--- src/main/java/frc/robot/subsystems/Roll.java
+++ src/main/java/frc/robot/subsystems/Roll.java
@@ -11,7 +11,7 @@ import frc.helpers.CCSparkMax;
 import frc.maps.Constants;
 
 public class Roll extends SubsystemBase{
-
+    //probably would want to add something regarding to roll for a certain distance so that the barrel lines up with the pneumatics
     CCSparkMax roll = new CCSparkMax("Roll", "r", Constants.MotorConstants.ROLL, MotorType.kBrushless, IdleMode.kBrake, Constants.MotorConstants.ROLL_REVERSE);
     
     public void rolling (double speed){
