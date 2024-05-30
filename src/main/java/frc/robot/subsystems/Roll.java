65d1080e4d94854d4597714a8b01e22c88d009c0
RuhanRajasthan <rsharma@cherrycreekschools.org>
2024-05-30 10:29:00 -0600

    Created Roll.java, added functions and command

---
+++ src/main/java/frc/robot/subsystems/Roll.java
@@ -0,0 +1,24 @@
+package frc.robot.subsystems;
+
+import java.util.function.DoubleSupplier;
+
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.helpers.CCSparkMax;
+import frc.maps.Constants;
+
+public class Roll extends SubsystemBase{
+
+    CCSparkMax roll = new CCSparkMax("Roll", "r", Constants.MotorConstants.ROLL, MotorType.kBrushless, IdleMode.kBrake, Constants.MotorConstants.ROLL_REVERSE);
+    
+    public void rolling (double speed){
+        roll.set(speed*.25);
+    }
+    
+    public Command doRoll (double speed){
+        return this.run(()-> rolling(speed));
+    }
+}
