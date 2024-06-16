7b6034bb3e4a5c00e7cab5371467cc69cf701d3e
RuhanRajasthan <rsharma@cherrycreekschools.org>
2024-06-16 16:30:48 -0600

    Create Pitch.java functions and  commands

---
+++ src/main/java/frc/robot/subsystems/Pitch.java
@@ -0,0 +1,25 @@
+package frc.robot.subsystems;
+
+import java.util.function.DoubleSupplier;
+
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.helpers.CCSparkMax;
+import frc.maps.Constants;
+
+
+public class Pitch extends SubsystemBase {
+    public CCSparkMax pitch = new CCSparkMax ("pitch" , "p", Constants.MotorConstants.PITCH, MotorType.kBrushless, IdleMode.kBrake, Constants.MotorConstants.PITCH_REVERSE);
+
+    public void doPitch(double speed){
+        pitch.set(speed*0.25);
+    }
+
+    public Command executePitch (double speed){
+        return this.run(()-> {doPitch(speed)});
+    }
+
+}
